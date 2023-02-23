40ac17677201a2a45e7d26e91f9032a84fcfdd2e
ArturoPedraza1 <125940774+ArturoPedraza1@users.noreply.github.com>
2023-02-22 19:54:46 -0600

    Add files via upload

---
+++ CuadroDeGato.java
@@ -0,0 +1,17 @@
+public class CuadroDeGato {
+
+        public  static  void  main ( String [] args ){
+
+System.out.println("------------------");
+System.out.println("|     |     |    |");
+System.out.println("|     |     |    |");
+System.out.println("------------------");
+System.out.println("|     |     |    |");
+System.out.println("|     |     |    |");
+System.out.println("------------------");
+System.out.println("|     |     |    |");
+System.out.println("|     |     |    |");
+System.out.println("------------------");
+
+}
+}
